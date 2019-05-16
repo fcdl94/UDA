4b6e13912027158381d3b21f08eed87930f42f10
fcdl94 <S203813s3>
2019-05-17 00:09:28 +0200

    Fix few errors

--- new.py
+++ new.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     method = Method(net, total_steps, device, AD=args.D, AY=args.Y, Td=args.T)
 
     print("Do a validation before starting to check it is ok...")
-    val_loss, val_acc = valid(net, valid_loader=test_loader)
+    val_loss, val_acc = valid(method, valid_loader=test_loader)
     print(f"Epoch {-1:03d} : Test Loss {val_loss:.6f}, Test Acc {val_acc:.2f}")
     print("Result should be random guessing, i.e. 10% accuracy")
 
@@ -116,7 +116,7 @@ if __name__ == '__main__':
         train_loss, train_acc, dom_loss, class_loss = train_epoch(method, source_loader, target_loader)
 
         # valid!
-        val_loss, val_acc = valid(net, valid_loader=test_loader)
+        val_loss, val_acc = valid(method, valid_loader=test_loader)
         print(f"Epoch {epoch + 1:03d} : Test Loss {val_loss:.6f}, Test Acc {val_acc:.2f}")
 
         log.log_training(epoch, train_loss, train_acc, val_loss, val_acc, dom_loss, class_loss)
@@ -127,7 +127,7 @@ if __name__ == '__main__':
             best_epoch = epoch
             best_model = torch.save(net.state_dict(),  save_name)
 
-    val_loss, val_acc = valid(net, valid_loader=test_loader, conf_matrix=True, log=log, n_classes=n_classes)
+    val_loss, val_acc = valid(method, valid_loader=test_loader, conf_matrix=True, log=log, n_classes=n_classes)
     with open('results.csv', 'a') as file:
         file.write(f"{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')},{setting},{method},{EPOCHS},{val_loss},{val_acc},{best_epoch},{best_val_loss},{best_val_acc}\n")
 
