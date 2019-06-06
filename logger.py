36f40bd93b1160f2f063acffc5b520b6ad63316d
fcdl94 <S203813s3>
2019-06-06 11:05:01 +0200

    Added test for shared classes

--- logger.py
+++ logger.py
@@ -1,6 +1,8 @@
 import numpy as np
 from tensorboardX import SummaryWriter
 import matplotlib.pyplot as plt
+import torch
+from sklearn.manifold import TSNE
 
 
 class TensorboardXLogger:
@@ -55,3 +57,40 @@ class TensorboardXLogger:
         avg_acc = np.diag(cm).mean() * 100.
         print(f"Per class accuracy: {avg_acc}")
         return conf
+
+    def print_tnse(self, method, test_loader, name):
+        # compute embeddings
+        outputs = []
+        feats = []
+        targets = []
+
+        with torch.no_grad():
+            for inputs, target in test_loader:
+                inputs = inputs.to(method.device)
+
+                output, feat = method.forward(inputs)
+
+                outputs.append(output.cpu())
+                feats.append(feat.cpu())
+                targets.append(target)
+
+        embeddings = torch.cat(outputs)
+        feats = torch.cat(feats)
+        labels= torch.cat(targets)
+
+        # make tsne of embeddings (from train and test)
+
+        X = embeddings.numpy()
+        y = labels.numpy()
+
+        print(X.shape)
+
+        tsne = TSNE()
+        X_embedded = tsne.fit_transform(X)
+
+        X_p = X_embedded
+        fig, ax = plt.subplots()
+        im = ax.scatter(X_p[:, 0], X_p[:, 1], c=y, cmap=plt.get_cmap('tab10'), s=8, alpha=0.8)
+        ax.figure.colorbar(im, ax=ax)
+
+        self.writer.add_figure(name, fig, close=True)
